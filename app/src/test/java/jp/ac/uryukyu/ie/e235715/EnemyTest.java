df5003db41b05acdae2af0f21acbdd38c82b2e60
Yuma Maeda <e235715@ie.u-ryukyu.ac.jp>
2023-11-15 17:44:45 +0900

    3rd commit

--- app/src/test/java/jp/ac/uryukyu/ie/e235715/EnemyTest.java
+++ app/src/test/java/jp/ac/uryukyu/ie/e235715/EnemyTest.java
@@ -13,6 +13,7 @@ class EnemyTest {
             demoHero.attack(slime); // over kill
         }
         slime.attack(demoHero);
-        assertEquals(defaultHeroHp, demoHero.hitPoint);
+        assertEquals(defaultHeroHp, demoHero.getHitPoint());
     }
 }
+
