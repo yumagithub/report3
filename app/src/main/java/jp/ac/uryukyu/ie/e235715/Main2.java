d57ac68efddaa217abc6bcd2f3184193e198d2be
Yuma Maeda <e235715@ie.u-ryukyu.ac.jp>
2023-11-28 11:35:33 +0900

    report4 1st commit

---
+++ app/src/main/java/jp/ac/uryukyu/ie/e235715/Main2.java
@@ -0,0 +1,20 @@
+package jp.ac.uryukyu.ie.e235715;
+
+public class Main2 {
+    public static void main(String[] args){
+        Hero hero = new Hero("勇者", 10, 5); // (a)
+        System.out.println(hero);
+        Enemy enemy = new Enemy("スライム", 6, 3);
+
+        System.out.printf("%s vs. %s\n", hero.getName(), enemy.getName());
+
+        int turn = 0;
+        while( hero.isDead() == false && enemy.isDead() == false ){
+            turn++;
+            System.out.printf("%dターン目開始！\n", turn);
+            hero.attack(enemy); // (b)
+            enemy.attack(hero);
+        }
+        System.out.println("戦闘終了");
+    }
+}
